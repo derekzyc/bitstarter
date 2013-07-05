c6e527fba46fe2b05fd5c3804168a4c8cc3d87e6
Ubuntu <ubuntu@ip-172-31-7-160.us-west-2.compute.internal>
2013-07-05 15:45:19 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   // response.send('Hello World 2!');
   var out = fs.readFileSync("index.html");
   response.send(out.toString());
-);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
